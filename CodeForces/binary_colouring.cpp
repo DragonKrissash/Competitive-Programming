815f358abff53f4ddb39541685bb219432ac93b4
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-05-27 17:57:45 +0530

    27-5-2024

--- CodeForces/binary_colouring.cpp
+++ CodeForces/binary_colouring.cpp
@@ -21,6 +21,23 @@ int main(){
             
             if(pow(2,power)-x < x-pow(2,power-1))
             {
+                power=power-1;
+                int arr[power+1]={0};
+                arr[power]=1;
+                int rem=x-pow(2,power);
+                for(int a=0;a<power && rem!=0;a++){
+                    if(pow(2,a)<=rem){
+                        rem=rem-pow(2,a);
+                        arr[a]=1;
+                    }
+                }
+                cout<<power+1<<"\n";
+            for(int a=0;a<power+1;a++)
+            cout<<arr[a]<<" ";
+            cout<<"\n";
+            }
+            else
+            {
             int arr[power+1]={0};
             int rem=pow(2,power)-x;
             arr[power]=1;
@@ -38,23 +55,6 @@ int main(){
             cout<<arr[a]<<" ";
             cout<<"\n";
             }
-            else{
-                power=power-1;
-                int arr[power+1]={0};
-                arr[power]=1;
-                int rem=x-pow(2,power);
-                for(int a=0;a<power && rem!=0;a++){
-                    if(pow(2,a)<=rem){
-                        rem=rem-pow(2,a);
-                        arr[a]=1;
-                    }
-                }
-                cout<<power+1<<"\n";
-            for(int a=0;a<power+1;a++)
-            cout<<arr[a]<<" ";
-            cout<<"\n";
-            }
-
             
         }
     }
