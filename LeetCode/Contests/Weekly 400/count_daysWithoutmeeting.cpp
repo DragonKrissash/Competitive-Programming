1f5261b08dd201778bbb91251b460ace6f3f766d
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-06-02 11:26:39 +0530

    2-6-2024

--- LeetCode/Contests/Weekly 400/count_daysWithoutmeeting.cpp
+++ LeetCode/Contests/Weekly 400/count_daysWithoutmeeting.cpp
@@ -2,23 +2,35 @@
 using namespace std;
 typedef long long ll;
 
-int countDays(int days, vector<vector<int>>& meetings) {
-    
-    bool arr[days+1]={false};
-    for(int i=0;i<meetings.size();i++){
-        if (arr[meetings[i][0]]!=true && arr[meetings[i][1]]!=true)
-        for(int j=meetings[i][0];j<=meetings[i][1];j++){
-            arr[j]=true;
+vector<vector<int>> merge(vector<vector<int>>&v){
+    vector<vector<int>> ans;
+    sort(v.begin(),v.end());
+    int n=v.size();
+    int s=v[0][0],e=v[0][0];
+    int si,ei;
+    for(int i=1;i<n;i++){
+        si=v[i][0];ei=v[i][1];
+        if(si>e){
+            ans.push_back({s,e});
+            s=si;
         }
+        e=max(e,ei);
     }
+    ans.push_back({s,e});
+    return ans;
+}
+
+int countDays(int days, vector<vector<int>>& meetings) {
     int c=0;
-    for(int i=1;i<=days;i++){
-        if(arr[i]==false)
-        c++;
+    vector<vector<int>> sortedmeetings=merge(meetings);
+    int busy=0;
+    for(auto meet:sortedmeetings){
+        busy+=meet[1]-meet[0]+1;
     }
-    return c;
+    return days-busy;
 
     }
+    
 
 int main()
 { ios_base::sync_with_stdio(false);
