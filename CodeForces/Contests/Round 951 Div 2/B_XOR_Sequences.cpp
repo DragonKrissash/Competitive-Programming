0d8b6df51477e0861e38ae050e1abbc0b7b61bb8
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-06-07 15:54:28 +0530

    7-6-2024

--- CodeForces/Contests/Round 951 Div 2/B_XOR_Sequences.cpp
+++ CodeForces/Contests/Round 951 Div 2/B_XOR_Sequences.cpp
@@ -1,39 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fo(i,n)   for(i=0;i<(n);++i)
-#define repA(i,j,n)   for(i=(j);i<=(n);++i)
-#define repD(i,j,n)   for(i=(j);i>=(n);--i)
 #define all(x) begin(x), end(x)
 #define sz(x) ((lli)(x).size())
-#define eb emplace_back
 #define X first
 #define Y second
-using lli = long long int;
-using mytype = long double;
-using ii = pair<lli,lli>;
-using vii = vector<ii>;
-using vi = vector<lli>;
+using vi=vector<int>;
+using ll=long long;
 int main()
 { ios_base::sync_with_stdio(false);
 cin.tie(NULL);
     int t;cin>>t;
     while(t--){
-        long long x,y;
+        int x,y;
         cin>>x>>y;
-        long long length=0;
-        bool istru=false;
-        vector<long long>xv(10000,0);
-        vector<long long>yv(10000,0);
-        for(long long a=1;a<10000;a++)
+        int temp=x^y;
+        int ind=1;
+        while(temp%2==0)
         {
-            xv[a-1]=x^a;
-            yv[a-1]=y^a;
+            temp=temp>>1;
+            ind=ind<<1;
         }
-        for(int a=1;a<10000;a++)
-        {       if(xv[a-1]==yv[a-1])
-            length++;
-        }
-        cout<<length<<"\n";
+        cout<<ind<<"\n";
     }
 
 }
