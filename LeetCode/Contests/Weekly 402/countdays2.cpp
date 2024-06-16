ad1bc139650c013655160e729c7820fb3bb5a8d6
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-06-16 10:26:29 +0530

    16-6-2024

---
+++ LeetCode/Contests/Weekly 402/countdays2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) begin(x), end(x)
+#define X first
+#define Y second
+
+long long countCompleteDayPairs(vector<int>& hours) {
+        sort(hours.begin(),hours.end());
+        int count=0;
+        for(int i=0;i<hours.size();i++){
+            
+        }
+    }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
---
+++ Misc/Mr.Mdodulo pairs.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) begin(x), end(x)
+#define X first
+#define Y second
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+vector<int> findDivs(int n){
+    vector<int> divs;
+    for(int i = 1; i <= sqrt(n); i++){
+        if(n % i == 0){
+            if(n/i==i)
+            divs.emplace_back(i);
+            else{
+                divs.emplace_back(i);
+                divs.emplace_back(n/i);
+            }
+        }
+    }
+    return divs;
+}
+
+int printPairs(int arr[], int n, int k)
+    {
+    	unordered_map<int, bool> map;
+        for(int a=0;a<n;a++){
+            map[arr[a]]=true;
+        }
+        int count=0;
+        for(int a=0;a<n;a++){
+            if(arr[a]>k && map[k]){
+                count++;
+            }
+            else{
+                if(arr[a]>=k){
+                    vector<int>v=findDivs(arr[a]);
+                    for(auto x:v){
+                        if(arr[a]%x==k and map[x])
+                        count++;
+                    }
+                    v.clear();
+                }
+            }       
+        }
+        map.clear();
+        return count;
+    }
+
+int main()
+{
+   fastio;
+
+}
